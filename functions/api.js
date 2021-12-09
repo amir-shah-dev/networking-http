6497e6a4d1dfba95fdde5df3923d339ef7709ba4
Amir Shah <axs@Amirs-MacBook-Air.local>
2021-12-09 22:23:27 +0000

    Initial Commit

---
+++ functions/api.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const serverless = require('serverless-http');
+
+const app = express();
+const router = express.Router();
+
+// router.get('/:operation/:num1/:num2', (req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/plain" });
+//     res.send(req.params);
+//     // res.end("Welcome");
+// });
+
+router.get('/:operation/:num1(\\d+)/:num2(\\d+)', function (req, res) {
+    // res.writeHead(200, { "Content-Type": "text/plain" });
+    if (req.params.operation === "add") {
+        res.set("Content-Type", "text/plain")
+        solution = Number(req.params.num1) + Number(req.params.num2)
+        res.send(String(solution))
+    } else if (req.params.operation === "subtract") {
+        res.set("Content-Type", "text/plain")
+        solution = Number(req.params.num1) - Number(req.params.num2)
+        res.send(String(solution))
+    } else if (req.params.operation === "multiply") {
+        res.set("Content-Type", "text/plain")
+        solution = Number(req.params.num1) * Number(req.params.num2)
+        res.send(String(solution))
+    } else if (req.params.operation === "divide") {
+        res.set("Content-Type", "text/plain")
+        solution = Number(req.params.num1) / Number(req.params.num2)
+        res.send(String(solution))
+    }
+    res.send(404);
+    // res.end("Welcome");
+});
+
+app.use('/', router)
+module.exports.handler = serverless(app);
