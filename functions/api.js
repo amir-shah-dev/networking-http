1261090c7524b1f1674f5be8bddad712a2951fdd
Amir Shah <axs@Amirs-MacBook-Air.local>
2021-12-10 08:33:03 +0000

    Revert fix attempt 8

--- functions/api.js
+++ functions/api.js
@@ -40,33 +40,33 @@ router.get('/:operation/:num1(\\d+)/:num2(\\d+)', function (req, res) {
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-// router.post('/', function (req, res) {
-//     var operation = req.body.operation;
-//     var arguments = req.body.arguments;
+router.post('/', function (req, res) {
+    var operation = req.body.operation;
+    var arguments = req.body.arguments;
 
-//     if (operation === "add") {
-//         res.set("Content-Type", "application/json")
-//         solution = Number(arguments[0]) + Number(arguments[1])
-//         res.json({"result": solution})
-//     } else if (operation === "subtract") {
-//         res.set("Content-Type", "application/json")
-//         solution = Number(arguments[0]) - Number(arguments[1])
-//         res.json({"result": solution})
-//     } else if (operation === "multiply") {
-//         res.set("Content-Type", "application/json")
-//         solution = Number(arguments[0]) * Number(arguments[1])
-//         res.json({"result": solution})
-//     } else if (operation === "divide") {
-//         res.set("Content-Type", "application/json")
-//         if (Number(arguments[1]) == 0) {
-//             res.json({"result": null})
-//         }
-//         solution = Number(arguments[0]) / Number(arguments[1])
-//         res.json({"result": solution})
-//     } else {
-//         res.send(404);
-//     }
-// });
+    if (operation === "add") {
+        res.set("Content-Type", "application/json")
+        solution = Number(arguments[0]) + Number(arguments[1])
+        res.json({"result": solution})
+    } else if (operation === "subtract") {
+        res.set("Content-Type", "application/json")
+        solution = Number(arguments[0]) - Number(arguments[1])
+        res.json({"result": solution})
+    } else if (operation === "multiply") {
+        res.set("Content-Type", "application/json")
+        solution = Number(arguments[0]) * Number(arguments[1])
+        res.json({"result": solution})
+    } else if (operation === "divide") {
+        res.set("Content-Type", "application/json")
+        if (Number(arguments[1]) == 0) {
+            res.json({"result": null})
+        }
+        solution = Number(arguments[0]) / Number(arguments[1])
+        res.json({"result": solution})
+    } else {
+        res.send(404);
+    }
+});
 
 app.use('/', router);
 module.exports.handler = serverless(app);
